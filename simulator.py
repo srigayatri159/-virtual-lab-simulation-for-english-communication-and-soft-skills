9eabbfebb553e0a7e0c3690ab75d6acfde331501
srigayatri159 <srigayatridatla.p@gmail.com>
2025-11-11 14:53:44 +0530

    Create ScenarioSimulator class in simulator.py

---
+++ simulator.py
@@ -0,0 +1,7 @@
+# simulator.py
+class ScenarioSimulator:
+    def __init__(self, scenario_script):
+        self.scenario = scenario_script
+    def run(self):
+        # Implement scenario logic
+        pass
